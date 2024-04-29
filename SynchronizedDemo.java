3029168eb3b5be89de56bd5454000c68d21d9d91
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-04-29 19:15:06 +0530

    Add files via upload

---
+++ SynchronizedDemo.java
@@ -0,0 +1,44 @@
+//we get regular output
+
+class Display
+{
+public synchronized void wish(String name)//using synch.method
+{
+for(int i=0;i<10;i++)
+{
+System.out.print("Good Morning:");
+try{
+Thread.sleep(2000);
+}
+catch(InterruptedException e)
+{
+}
+System.out.println(name);
+}
+}
+}
+class MyThread extends Thread
+{
+Display d;
+String name;
+MyThread(Display d,String name)
+{
+this.d=d;
+this.name=name;
+}
+public void run()
+{
+d.wish(name);
+}
+}
+class SynchronizedDemo
+{
+public static void main(String [] args)
+{
+Display d=new Display();
+MyThread t=new MyThread(d,"pyl");
+MyThread t1=new MyThread(d,"adesh");
+t.start();
+t1.start();
+}
+}
---
+++ SynchronizedDemo3.java
@@ -0,0 +1,45 @@
+//is prgm. mein hume regrular output milega kunki isme hume static synchronized method ka use krna pdega (jo threads hain woh different java objects ko operate ke rahe hain )
+
+class Display
+{
+public static synchronized void wish(String name)//we get regular output
+{
+for(int i=0;i<10;i++)
+{
+System.out.print("Good Morning:");
+try{
+Thread.sleep(2000);
+}
+catch(InterruptedException e)
+{
+}
+System.out.println(name);
+}
+}
+}
+class MyThread extends Thread
+{
+Display d;
+String name;
+MyThread(Display d,String name)
+{
+this.d=d;
+this.name=name;
+}
+public void run()
+{
+d.wish(name);
+}
+}
+class SynchronizedDemo3
+{
+public static void main(String [] args)
+{
+Display d1=new Display();//----1 d1 is one java object
+Display d2=new Display();//----2 d2 is another java object
+MyThread t=new MyThread(d1,"pyl");//operate d1 jave object
+MyThread t1=new MyThread(d2,"adesh");//operate d2 java object
+t.start();
+t1.start();
+}
+}
